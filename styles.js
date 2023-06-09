88d3065d41a59df3053390d5c3f68d4bf766bd0e
redx-mistry-k <67650012+redx-mistry-k@users.noreply.github.com>
2023-06-09 14:54:02 +0100

    Update styles.js

--- styles.js
+++ styles.js
@@ -1,1 +1,18 @@
+const mobileMenu = document.getElementById('mobile-menu');
+const navbarMenu = document.querySelector('.navbar__menu');
+
+mobileMenu.addEventListener('click', function () {
+  mobileMenu.classList.toggle('active');
+  navbarMenu.classList.toggle('active');
+});
+
+// Close the mobile menu when a menu item is clicked
+const navbarLinks = document.querySelectorAll('.navbar__links');
+
+navbarLinks.forEach(function (link) {
+  link.addEventListener('click', function () {
+    mobileMenu.classList.remove('active');
+    navbarMenu.classList.remove('active');
+  });
+});
 
