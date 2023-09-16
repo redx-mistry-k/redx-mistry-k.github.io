9f93be07a5699c60cfb5ccf54e0531733619576c
redx-mistry-k <67650012+redx-mistry-k@users.noreply.github.com>
2023-09-16 18:37:04 +0100

    Add files via upload

---
+++ phptest.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My PHP Website</title>
+</head>
+<body>
+    <h1>Welcome to My PHP Website</h1>
+    <p>This is a basic PHP web page.</p>
+
+    <?php
+    // Your PHP code goes here
+    echo "Today is " . date("Y-m-d") . "<br>";
+    echo "This page is powered by PHP!";
+    ?>
+</body>
+</html>
